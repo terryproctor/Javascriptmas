75cb609f9e5eae3155a7e03879e002a072979933
terryproctor <terryproctor2015@outlook.com>
2022-12-11 19:36:37 +0000

    one line-sort with compare function on word length

--- Day 10/order by length.js
+++ Day 10/order by length.js
@@ -1,41 +1,24 @@
 function sortByLength(strs) {
-    
-    function createWordLength(word) {
-   let wordObject = {};
-   wordObject[word] = word.length;
-   return wordObject;
-   };
-
-   
-    let strObjectArray = [];
-   
-    for (let i=0; i<strs.length; i++) {
-        let str = strs[i]; 
-        strObjectArray.push(createWordLength(str));
-    }; 
-    return strObjectArray;
+    return strs.sort(function(a, b){return a.length -b.length;});
 }
 
 
 
-// /**
-// * Test Suite 
-
+/**
+* Test Suite 
+*/
 describe('sortByLength()', () => {
-   it('sorts the strings from shortest to longest', () => {
-       // arrange
-       const strs = ["abc", "", "aaa", "a", "zz"];
-       
-       // act
-       const result = sortByLength(strs);
+it('sorts the strings from shortest to longest', () => {
+    // arrange
+    const strs = ["abc", "", "aaa", "a", "zz"];
+    
+    // act
+    const result = sortByLength(strs);
 
-       // log
-       console.log("result: ", result);
-       
-       // assert
-       expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
-   });
+    // log
+    console.log("result: ", result);
+    
+    // assert
+    expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
 });
-
-
-
+});
