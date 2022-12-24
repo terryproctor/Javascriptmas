e79480088132d98cbacb6585368f27498855b801
terryproctor <terryproctor2015@outlook.com>
2022-12-24 23:04:10 +0000

    backround colour of playing

--- Day 24/index.js
+++ Day 24/index.js
@@ -5,4 +5,14 @@ function playSong(id) {
   let player_src = `https://www.youtube.com/embed/${id}?autoplay=1`;
   let player = document.getElementById('player');
   player.src = player_src;
+  let playing = document.querySelector(`[onclick="playSong('${id}')"]`);
+  let buttons = document.getElementsByTagName("button");
+  console.log(buttons[2]);
+  for (let i = 0; i < buttons.length; i++){
+    buttons[i].style.backgroundColor = "#BDD59B";
+  }
+  // buttons.forEach(element => {
+  //   element.style.backgroundColor = "red";    
+  // });
+  playing.style.backgroundColor = "blue";
 }
